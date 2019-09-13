d5d40016c136806a3123a28facf7f29e7f1c54af
jgriffin <jeremy.griffin@movehq.com>
2019-09-13 13:47:07 -0400

    Feat: ISS-04 - Bring together to show results
    Bring the three branches together so the form goes to the processor and
    the results are displayed on the index page.
    
    For: ISS-04
    
    On branch rel01-sp01-iss04
        Modified:   comments.html
        Modified:   commentsProcessor.php
        Typechange: index.html
        New file:   index.php

--- commentsProcessor.php
+++ commentsProcessor.php
@@ -1,20 +1,30 @@
 <?php
 
-$results = $_REQUEST['comment'];
+$comment = $_REQUEST['comment'];
 
 $filepath = __DIR__ . '/comments/';
 
-if (! file_exists($filepath)) {
-    $mkdirResult = mkdir($filepath);
-    if (!$mkdirResult) {
-        throw new Exception('Failed to create comments directory.', 10001);
+try {
+    if (!file_exists($filepath)) {
+        $mkdirResult = mkdir($filepath);
+        if (!$mkdirResult) {
+            throw new Exception('Failed to create comments directory.', 10001);
+        }
     }
+    $filename = time() . getmypid();
+    $wroteCount = file_put_contents($filepath.$filename, $comment);
+    $response = json_encode([
+                              'success' => true,
+                              'wroteCount' => $wroteCount
+                          ]);
+} catch (Exception $exception) {
+    $response = json_encode([
+                              'success' => false,
+                              'errorMessage' => $exception->getMessage(),
+                              'errorCode' => $exception->getCode()
+                          ]);
 }
 
-$filename = time() . getmypid();
-
-$wroteCount = file_put_contents($filepath.$filename, $results);
-
-print json_encode([wroteCount => $wroteCount]);
+header('Location: /index.php?response='. $response);
 
 exit();
