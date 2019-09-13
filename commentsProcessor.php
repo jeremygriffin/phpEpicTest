d67b57029ee7aba45a7072f395d824c64b19b3fb
jgriffin <jeremy.griffin@movehq.com>
2019-09-13 09:20:12 -0400

    Fix: ISS-02 - Make sure the comments dir exists
    The comments dir didn't exist so the file wasn't saved.
    
    For: ISS-02
    
    On branch rel01-sp01-iss02
        New file:   comments/.gitkeep
        Modified:   commentsProcessor.php

--- commentsProcessor.php
+++ commentsProcessor.php
@@ -3,6 +3,14 @@
 $results = $_REQUEST['comment'];
 
 $filepath = __DIR__ . '/comments/';
+
+if (! file_exists($filepath)) {
+    $mkdirResult = mkdir($filepath);
+    if (!$mkdirResult) {
+        throw new Exception('Failed to create comments directory.', 10001);
+    }
+}
+
 $filename = time() . getmypid();
 
 $wroteCount = file_put_contents($filepath.$filename, $results);
